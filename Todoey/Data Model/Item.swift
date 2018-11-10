fabcf4c967a593d8632b9036275782259f38c644
Adam Peterson <adam@cultivatedwit.com>
2018-11-10 13:09:57 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Adam on 11/10/18.
+//  Copyright © 2018 CuriousPeterson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
